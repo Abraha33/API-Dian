b8b7ecc38e5dc7af4043202308fd2d40692ec6fa
Abraha33 <168048145+Abraha33@users.noreply.github.com>
2026-08-18 23:56:23 -0500

    feat: wire fake provider into worker module

---
+++ apps/api/src/modules/worker/worker.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { FakeFiscalProvider } from '../provider/fake-fiscal-provider';
+import {
+  FISCAL_PROVIDER,
+  type FiscalProvider,
+} from '../provider/fiscal-provider';
+import { FiscalWorkerRepository } from './fiscal-worker.repository';
+import { FiscalWorkerService } from './fiscal-worker.service';
+
+@Module({
+  providers: [
+    FiscalWorkerRepository,
+    {
+      provide: FISCAL_PROVIDER,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService): FiscalProvider =>
+        new FakeFiscalProvider(
+          config.getOrThrow('FAKE_PROVIDER_SCENARIO'),
+        ),
+    },
+    FiscalWorkerService,
+  ],
+  exports: [FiscalWorkerRepository, FiscalWorkerService],
+})
+export class WorkerModule {}
